089c5a1dd39471fb3bf1667466b8eaeac0ef0526
Prxfful <praffulhadpad52@gmail.com>
2025-11-04 15:01:44 +0530

    Create Read-operation with array.c

---
+++ Read-operation with array.c
@@ -0,0 +1,13 @@
+//read operatiom using Array
+#include<stdio.h>
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n = sizeof(arr)/ sizeof(arr[0]);
+    int index=3;
+    if(index>=0 && index<n){
+        int val = arr[index];
+        printf("a[%d]= %d",index, val);
+    }
+}
+output:
+a[3]= 4
